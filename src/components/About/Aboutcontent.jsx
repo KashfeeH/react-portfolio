4119a95e67c63531fa65a89e211deb8a66b62b95
KashfeeH <151862358+KashfeeH@users.noreply.github.com>
2024-03-07 20:12:37 +0000

    trying to fix the pdf download

--- src/components/About/Aboutcontent.jsx
+++ src/components/About/Aboutcontent.jsx
@@ -1,7 +1,5 @@
 import styles from '../About/Aboutcontent.module.css'
-import { Link } from 'react-router-dom'
 import React from 'react'
-import myresume from '../../../assets/resume/myresume.pdf'
 import skills from '../../data/skills.json'
 import { getImageUrl } from "../../utils";
 
@@ -10,9 +8,9 @@ const Aboutcontent = () => {
   <section className={styles.about}>
       <div className={styles.left}>
         <h3> Learn more about me</h3>
-      <Link to ='../../../assets/resume/myresume.pdf' target='_blank' download="myresume.pdf">
-       <button className={styles.btn}>Download resume</button>        
-      </Link>
+        <a href='myresume.pdf' download="myresume.pdf">
+        <button className={styles.btn}>Download resume</button>        
+        </a>
       </div>
     <div className={styles.right}>
     <h3>Skills</h3>
